610cc579977327e7ede9efb15658eb7ad240c7b9
Matt Simerson <matt@tnpi.net>
2026-05-11 21:02:55 -0700

    add author & contribution attribution
    convert Changes and README to markdown

--- lib/Qmail/Deliverable.pm
+++ lib/Qmail/Deliverable.pm
@@ -538,5 +538,24 @@ LICENSE file to reflect the license(s) they chose to redistribute under.
 
 =head1 AUTHORS
 
-Matt Simerson <msimerson@cpan.org>
-Juerd Waalboer <#####@juerd.nl>
+=over 4
+
+=item *
+
+Juerd Waalboer <#####@juerd.nl> (original author)
+
+=item *
+
+Matt Simerson <msimerson@cpan.org> (current maintainer)
+
+=back
+
+=head1 CONTRIBUTORS
+
+=over 4
+
+=item *
+
+Martin Sluka (1.09)
+
+=back
